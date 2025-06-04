2ecf41bcdb50c2a4801f3a65efb961fc53ea84c0
adrian-refck <adrian.refck@gmx.de>
2025-06-04 13:36:09 +0000

    brtest4.2

--- app/main.py
+++ app/main.py
@@ -5,6 +5,33 @@ from links import FROH, TRAURIG
 
 st.title("Eisenbahntest")
 
+#hintergrundbild mit url
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://www.br.de/berge/wandern/04_aussicht-vom-edelsberg-100~_v-img__16__9__xl_-d31c35f8186ebeb80b0cd843a7c267a0e0c81647.jpg?version=d7cc0");
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 wahl = st.selectbox("Wähle deinen beforzugten Test:", ["Bitte wählen" , "Eisenbahnen in der DDR", "Moderne Deutschen Eisenbahnen"])
 if wahl == "Eisenbahnen in der DDR":
     st.image("https://y7b2e7b4.delivery.rocketcdn.me/wp-content/uploads//2019/09/6-Triebwagen-VT172-2.jpg" , width=1000) 
@@ -33,10 +60,22 @@ if wahl == "Eisenbahnen in der DDR":
                             st.header("Du hast den Test bestanden")
                             st.video("https://youtu.be/WQYN2P3E06s", autoplay=True)
 
-else:
+if wahl == "Moderne Deutschen Eisenbahnen":
     st.write("Tolle Wahl")
     st.image("https://www.bahnbilder.de/1200/karwendelmassivaus-innsbruck-kommend-rollt-2442-1089108.jpg", width=1000) 
-    auswahl7 = st.selectbox("Wähle die Br:", ["Br 2442" , "Br 423", "Br 146" , "Br 101", "Br 145"])
+    auswahl7 = st.selectbox("Wähle die Br:", ["Br 2441" , "Br 423", "Br 2442" , "Br 101", "Br 145"])
     if auswahl7 == "Br 2442":
         st.write("Du hast Recht")
         st.image("https://www.modelleisenbahnanlage.com/wp-content/uploads/2009/02/br103-rot.jpg", width=1000)
+        auswahl8 = st.selectbox("Wähle die Br:", ["Br 101" , "Br 145", "Br 423", "Br 2441", "Br 103"])
+        if auswahl8 == "Br 103":
+                st.write("Du hast Recht")
+                st.image("https://image.jimcdn.com/app/cms/image/transf/dimension=500x10000:format=jpg/path/s39c9c999a872678e/image/i2794e3dd1d5e995d/version/1725286073/image.jpg" , width=1000)
+                auswahl9 = st.selectbox("Wähle die Br:", ["Br 109" , "Br 101", "Br 423", "Br 2441", "Br 201"])
+                if auswahl9 == "Br 101":
+                    st.write("Super")
+                    st.image("https://www.zukunft-mobilitaet.net/wp-content/uploads/2011/05/icx-br412-testfahrten-db-ice4-deutsche-bahn-fernverkehr-siemens-bild.jpg" , width=1000)
+                    auswahl10 = st.selectbox("Wähle die Br:", ["Br 201" , "Br 412", "Br 2441", "Br 109", "Br 145"])
+                    if auswahl10 == "Br 412":
+                        st.write("Genau")
+                        st.image("https://www.larsbrueggemann.de/fotos-eb-28-630px/529ewd-098-foto-ice-l-lauenbrueck.jpg")
